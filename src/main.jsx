1df07f43ca79184855be6aa71b7a35d4bcb8bed5
SaraElsayed <esara7772@gmail.com>
2026-08-14 16:41:19 +0300

    add router and styles

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,35 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import AboutPage from './pages/AboutPage'
+import ProductPage from './pages/ProductPage'
+import LoginForm from './components/loginform'
+import ProductsPage from './pages/ProductsPage.jsx'
+import Signup from './components/signup'
+import Wishlist from './components/wishlist'
+import ErrorPage from './pages/ErrorPage.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <HomePage />, errorElement: <ErrorPage /> },
+      { path: 'about', element: <AboutPage />, errorElement: <ErrorPage /> },
+      { path: 'products', element: <ProductsPage />, errorElement: <ErrorPage /> },
+      { path: 'product/:id', element: <ProductPage />, errorElement: <ErrorPage /> },
+      { path: 'login', element: <LoginForm />, errorElement: <ErrorPage /> },
+      { path: 'signup', element: <Signup />, errorElement: <ErrorPage /> },
+      { path: 'wishlist', element: <Wishlist />, errorElement: <ErrorPage /> },
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
